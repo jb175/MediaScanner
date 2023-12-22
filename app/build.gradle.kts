b5f3c0472207de473afda1e057958132290c5209
Jean-Baptiste Moufle <jb.moufle@gmail.com>
2023-12-22 15:48:53 +0100

    add login screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -53,11 +56,16 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
     implementation("androidx.room:room-runtime:2.6.1")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation("androidx.annotation:annotation:1.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-analytics:21.5.0")
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
 
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/fr/isep/mediascanner/fragment/AccountFragment.kt
+++ app/src/main/java/fr/isep/mediascanner/fragment/AccountFragment.kt
@@ -1,14 +1,39 @@
 package fr.isep.mediascanner.fragment
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import fr.isep.mediascanner.R
+import fr.isep.mediascanner.activity.ui.login.LoginActivity
 
 class AccountFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var currentUser: FirebaseUser
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+        if (auth.currentUser == null) {
+            val intent = Intent(context, LoginActivity::class.java)
+
+            this.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    //TODO
+                }
+            }.launch(intent)
+        }
     }
 }
